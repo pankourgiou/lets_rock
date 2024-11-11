f94431155ffa92d451383b1bf4c014e9c0b006dd
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 23:08:14 +0200

    Create lets_rock.swift

---
+++ lets_rock.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+// Start time
+let start = Date()
+
+// Code you want to measure
+print("Ayreon album 01011001 is super cool")
+// Add a delay or some code to test
+sleep(2)
+print("Reevaluation of Dream Theater band album artwork and booklets as NFTs")
+
+// End time
+let end = Date()
+
+// Calculate the time difference in seconds
+let timeInterval = end.timeIntervalSince(start)
+
+print("Time elapsed: \(timeInterval) seconds")

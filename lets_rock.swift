51697d9226bcf96070969001af8984156ff188b9
L <60981745+pankourgiou@users.noreply.github.com>
2025-01-15 07:58:23 +0200

    Update lets_rock.swift

--- lets_rock.swift
+++ lets_rock.swift
@@ -4,10 +4,11 @@ import Foundation
 let start = Date()
 
 // Code you want to measure
-print("Ayreon album 01011001 is super cool")
+print("Alpha Yankee Romeo Echo Oscar November   Alpha Lima Bravo Uniform Mike   Zero One Zero One One Zero Zero One   India Sierra   Sierra Uniform Papa Echo Romeo   Charlie Oscar Oscar Lima 
+")
 // Add a delay or some code to test
 sleep(2)
-print("Reevaluation of Dream Theater band album artwork and booklets as NFTs")
+print("Romeo Echo Echo Victor Alpha Lima Uniform Alpha Tango India Oscar November   Oscar Foxtrot   Delta Romeo Echo Alpha Mike   Tango Hotel Echo Alpha Tango Echo Romeo   Bravo Alpha November Delta   Alpha Lima Bravo Uniform Mike   Alpha Romeo Tango Whiskey Oscar Romeo Kilo   Alpha November Delta   Bravo Oscar Oscar Kilo Lima Echo Tango Sierra   Alpha Sierra   November Foxtrot Tango Sierra ")
 
 // End time
 let end = Date()
